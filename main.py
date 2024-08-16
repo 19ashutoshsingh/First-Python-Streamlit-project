63211e18def4d64c26d5bbf5bf54d7e774f97b7b
Ashutosh Singh <19ashutoshsingh@gmail.com>
2024-08-16 06:48:37 +0530

    Commin

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas
+
+data = {
+    'Series_1': [1, 15, 10, 5, 20],
+    'Series_2': [10, 60, 30, 70, 50]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything With Python')
 st.write('''This is our first Web App.
 Enjoy it!
 ''')
+
+st.write(df)
+st.line_chart(df)
