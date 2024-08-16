2804bae954bea84bcc6e10f3dca7b2b5779b2cab
Ashutosh Singh <19ashutoshsingh@gmail.com>
2024-08-16 06:29:12 +0530

    Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything With Python')
+st.write('''This is our first Web App.
+Enjoy it!
+''')
